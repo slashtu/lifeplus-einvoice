966680f195d8e2ee58dc30a9c6c077d69a3082fc
slash <y12341234@gmail.com>
2014-11-28 19:01:21 +0800

    1. css  2. sidebar event

--- static/css/js/einvoice.js
+++ static/css/js/einvoice.js
@@ -5,5 +5,12 @@ $(function() {
         $('#page-wrapper').toggleClass('col-md-12 col-md-9');
     });
 
+    $('#page-wrapper').click(function(){
+        var sidebar_class = $('#sidebar').attr('class');
+        if( sidebar_class.indexOf('lifeplus-sidebar-toggle') != -1 ){
+            $('#sidebar').toggleClass('lifeplus-sidebar lifeplus-sidebar-toggle');
+            $('#page-wrapper').toggleClass('col-md-12 col-md-9');
+        }
+    });
 });
 
